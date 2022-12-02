ea7ceb44adff6ff631b8d460743a2c8e7266a30f
mujahid-dzinemedia <106052326+mujahid-dzinemedia@users.noreply.github.com>
2022-12-02 16:27:30 +0500

    :+1:

--- smartcropperlib/src/main/java/me/pqpo/smartcropperlib/SmartCropper.java
+++ smartcropperlib/src/main/java/me/pqpo/smartcropperlib/SmartCropper.java
@@ -29,7 +29,8 @@ public class SmartCropper {
     }
 
     /**
-     *  输入图片扫描边框顶点
+     * 输入图片扫描边框顶点
+     *
      * @param srcBmp 扫描图片
      * @return 返回顶点数组，以 左上，右上，右下，左下排序
      */
@@ -50,7 +51,8 @@ public class SmartCropper {
 
     /**
      * 裁剪图片
-     * @param srcBmp 待裁剪图片
+     *
+     * @param srcBmp     待裁剪图片
      * @param cropPoints 裁剪区域顶点，顶点坐标以图片大小为准
      * @return 返回裁剪后的图片
      */
@@ -67,15 +69,63 @@ public class SmartCropper {
         Point leftBottom = cropPoints[3];
 
         int cropWidth = (int) ((CropUtils.getPointsDistance(leftTop, rightTop)
-                + CropUtils.getPointsDistance(leftBottom, rightBottom))/2);
+                + CropUtils.getPointsDistance(leftBottom, rightBottom)) / 2);
         int cropHeight = (int) ((CropUtils.getPointsDistance(leftTop, leftBottom)
-                + CropUtils.getPointsDistance(rightTop, rightBottom))/2);
+                + CropUtils.getPointsDistance(rightTop, rightBottom)) / 2);
 
         Bitmap cropBitmap = Bitmap.createBitmap(cropWidth, cropHeight, Bitmap.Config.ARGB_8888);
         SmartCropper.nativeCrop(srcBmp, cropPoints, cropBitmap);
         return cropBitmap;
     }
 
+    /**
+     * 裁剪图片
+     *
+     * @param srcBmp     待裁剪图片
+     * @param cropPoints 裁剪区域顶点，顶点坐标以图片大小为准
+     * @return 返回裁剪后的图片
+     */
+    public static Bitmap cropNewWay(Bitmap srcBmp, Point[] cropPoints) {
+        if (srcBmp == null || cropPoints == null) {
+            throw new IllegalArgumentException("srcBmp and cropPoints cannot be null");
+        }
+        if (cropPoints.length != 4) {
+            throw new IllegalArgumentException("The length of cropPoints must be 4 , and sort by leftTop, rightTop, rightBottom, leftBottom");
+        }
+
+        //纠正4个点的位置，将距离(0,0)最近的点作为cropPoints[0]
+        double minDistance = Integer.MAX_VALUE;
+        int index = -1;
+        int length = cropPoints.length;
+        for (int i = 0; i < length; i++) {
+            double distance2 = CropUtils.getPointsDistance(cropPoints[i], new Point(0, 0));
+            if (minDistance > distance2) {
+                minDistance = distance2;
+                index = i;
+            }
+        }
+        if (index > 0) {
+            Point[] resultCropPoint = new Point[length];
+            for (int i = 0; i < cropPoints.length; i++) {
+                resultCropPoint[i] = cropPoints[(index + i) >= length ? (index + i) - length : (index + i)];
+            }
+            cropPoints = resultCropPoint.clone();
+        }
+
+        Point leftTop = cropPoints[0];
+        Point rightTop = cropPoints[1];
+        Point rightBottom = cropPoints[2];
+        Point leftBottom = cropPoints[3];
+
+        int cropWidth = (int) ((CropUtils.getPointsDistance(leftTop, rightTop)
+                + CropUtils.getPointsDistance(leftBottom, rightBottom)) / 2);
+        int cropHeight = (int) ((CropUtils.getPointsDistance(leftTop, leftBottom)
+                + CropUtils.getPointsDistance(rightTop, rightBottom)) / 2);
+
+        Bitmap cropBitmap = Bitmap.createBitmap(cropWidth, cropHeight, Bitmap.Config.ARGB_8888);
+        SmartCropper.nativeCrop(srcBmp, cropPoints, cropBitmap);
+        return cropBitmap;
+    }
 
 
     private static native void nativeScan(Bitmap srcBitmap, Point[] outPoints, boolean canny);
